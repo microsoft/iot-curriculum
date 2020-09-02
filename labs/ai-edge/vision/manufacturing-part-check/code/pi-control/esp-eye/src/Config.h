44f65b397ce896be58f965934e83461e07ae3b35
Jim Bennett <jim.bennett@microsoft.com>
2020-09-02 13:44:27 -0700

    Update Config.h

--- labs/ai-edge/vision/manufacturing-part-check/code/pi-control/esp-eye/src/Config.h
+++ labs/ai-edge/vision/manufacturing-part-check/code/pi-control/esp-eye/src/Config.h
@@ -7,6 +7,11 @@
 const char * const ssid = "<Your SSID>";
 const char * const password = "<Your password>";
 
+// The prediction URL and key come from Custom Vision.
+// Set these to the value from your published iteration
+const char * const predictionUrl = "<Your prediction URL>";
+const char * const predictionKey = "<Your prediction key>";
+
 // The device connection string comes from Azure IoT Hub
 // Set these to the value for your device, not the Hub
 const char * const deviceConnectionString = "<Device connection string>";
